b2ea6ebfd17be875fa14979a32215af94958259e
David Xu <david.xu@duke.edu>
2022-06-10 13:38:48 -0400

    basically done, styling a bit off

--- src/Meme.js
+++ src/Meme.js
@@ -4,12 +4,36 @@ import "./App.css"
 
 function Meme() {
 
+    // * Challenge: 
+    //  * As soon as the Meme component loads the first time,
+    //  * make an API call to "https://api.imgflip.com/get_memes".
+    //  * 
+    //  * When the data comes in, save just the memes array part
+    //  * of that data to the `allMemes` state
+    //  * 
+    //  * Think about if there are any dependencies that, if they
+    //  * changed, you'd want to cause to re-run this function.
+    //  * 
+    //  * Hint: for now, don't try to use an async/await function.
+    //  * Instead, use `.then()` blocks to resolve the promises
+    //  * from using `fetch`. We'll learn why after this challenge.
+    //  */
+
     const [meme, setMeme] = React.useState({
         topText: "",
         bottomText: "",
         randomImage: "http://i.imgflip.com/1bij.jpg" 
     })
 
+    const [allMemes, setAllMemes] = React.useState([])
+
+    React.useEffect(function() {
+        fetch("https://api.imgflip.com/get_memes")
+        .then(res => res.json())
+        .then(data => setAllMemes(data.data.memes))
+    }, [])
+
+    
 
     const [memeImage, setMemeImage] = React.useState("http://i.imgflip.com/1bij.jpg")
     
--- src/useStatePractice.js
+++ src/useStatePractice.js
@@ -451,3 +451,59 @@ export default function Form() {
         </form>
     )
 }
+
+
+// import React from "react"
+// import WindowTracker from "./WindowTracker"
+
+// export default function App() {
+//     /**
+//      * Challenge:
+//      * 1. Create state called `show`, default to `true`
+//      * 2. When the button is clicked, toggle `show`
+//      * 3. Only display `<WindowTracker>` if `show` is `true`
+//      */
+    
+//     const [show, setShow] = React.useState(true)
+    
+//     function toggle() {
+//         setShow(prevShow => !prevShow)
+//     }
+    
+//     return (
+//         <div className="container">
+//             <button onClick={toggle}>
+//                 Toggle WindowTracker
+//             </button>
+//             {show && <WindowTracker />}
+//         </div>
+//     )
+// }
+
+
+
+
+import React from "react"
+
+export default function WindowTracker() {
+    /**
+     * Challenge:
+     * 1. Create state called `windowWidth`, default to 
+     *    `window.innerWidth`
+     * 2. When the window width changes, update the state
+     * 3. Display the window width in the h1 so it updates
+     *    every time it changes
+     */
+    
+    const [windowWidth, setwindowWidth] = React.useState(window.innerWidth)    
+    
+    React.useEffect(() => {
+        window.addEventListener("resize", function() {
+            setwindowWidth(window.innerWidth)
+        })
+    }, [])
+    
+    return (
+        <h1>Window width: {windowWidth}</h1>
+    )
+}
